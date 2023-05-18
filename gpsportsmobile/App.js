b3cc07a47e7017ddfcbfb81a83b00f9cd583f478
JIgorrr <joaoigor881@gmail.com>
2023-05-17 21:31:23 -0300

    Teste Commite

--- gpsportsmobile/App.js
+++ gpsportsmobile/App.js
@@ -19,7 +19,7 @@ export default function App() {
           translucent
         /> 
 
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <SignIn/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
